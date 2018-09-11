a0092a023ef05d55de87b2c1db5689376a59cdae
Vinicius <vi_saran@hotmail.com>
2018-09-10 21:06:12 -0300

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript'},
+        {title: 'Harry Potter'},
+        {title: 'The dark Tower'},
+        {title: 'Eloquent Day'}
+    ]
+}
